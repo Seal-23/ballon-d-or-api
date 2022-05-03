c4e3b5b2de18c9a9368a81d02302ef14272e77f3
Seal-23 <salarconlagos@gmail.com>
2022-05-02 21:02:32 -0500

    fix sonnar scan problem

--- src/winners/interfaces/winners.interface.ts
+++ src/winners/interfaces/winners.interface.ts
@@ -1,5 +1,4 @@
-import mongoose from 'mongoose';
-import { Document } from 'mongoose';
+import mongoose, { Document } from 'mongoose';
 
 export interface Player extends Document {
   readonly name: string;
